fc466c2860a0d92a58c8cad067234eb97e66372c
mario arriola <riblarsv@gmail.com>
2023-06-20 04:27:45 +0200

    task: finish my website api

---
+++ script.js
@@ -0,0 +1,25 @@
+
+const jokesContainer = document.getElementById('jokes-container')
+const newJoke = document.querySelector('#new-joke')
+
+newJoke.addEventListener('click',function(){
+    callToAPI()
+})
+
+function displayOnTheScreen(joke){
+    const jokeText = joke.value
+    jokesContainer.innerText = jokeText
+}
+
+async function callToAPI(){
+    try{
+    const req =  await fetch('https://api.chucknorris.io/jokes/random')
+    const res =  await req.json()
+    displayOnTheScreen(res)
+
+    }catch(e){
+        console.error()
+    }
+}
+
+
